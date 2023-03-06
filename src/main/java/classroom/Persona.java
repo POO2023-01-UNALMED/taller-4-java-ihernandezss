d55f0506e134eb5e674235b4d6c765100d7aae4e
ihernandezss <118498504+ihernandezss@users.noreply.github.com>
2023-03-05 20:07:25 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,47 +1,54 @@
 package classroom;
 
 public class Persona {
+	
 
-    long cedula;
-    String nombre;
-    static int totalPersonas;
-    
-    static {
-        totalPersonas = 0;
-    }
+	    long cedula=1;
+	    String nombre;
+	    static int totalPersonas;
+	    
+	    static {
+	        totalPersonas = 0;
+	    }
 
-    public Persona(long cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
-    
-    public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
+	    public Persona(long cedula, String nombre) {
+	        this.cedula = cedula;
+	        this.nombre = nombre;
+	        totalPersonas++;
+	    }
+	    public Persona() {
+	    	cedula=0;
+	    }
+	    
+	    public Persona(String nombre, long cedula) {
+	        this.cedula = cedula;
+	        this.nombre = nombre;
+	        totalPersonas++;
+	    }
 
-    public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
-        totalPersonas++;
-    }
+	    public Persona(long cedula) {
+	        this.cedula = cedula;
+	        this.nombre = "";
+	        totalPersonas++;
+	    }
 
-    public Persona(String nombre) {
-        this.nombre = "";
-        totalPersonas++;
-    }
-    
-    public long getCedula() {
-        return cedula;
-    }
+	    public Persona(String nombre) {
+	        this.nombre = "";
+	        cedula=1;
+	        totalPersonas++;
+	    }
+	    
+	    public long getCedula() {
+	        return cedula;
+	    }
 
-    public String getNombre() {
-        return nombre;
-    }
+	    public String getNombre() {
+	        return nombre;
+	    }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-}
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+	}
+
+
